b5ca3e106e74d7c00c0e1e952b0b7e9f0e3f8aba
vrsriar <vrsriar@gmail.com>
2020-12-13 19:11:51 +0100

    PB_21

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    AudioSource audioData;
-    Rigidbody rigidBody;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    
+    AudioSource audioData;
+    Rigidbody rigidBody;
+    
+
+    enum State { Alive, Dying, Transcending }
+    [SerializeField]  State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +23,41 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (state == State.Alive)
+        {
         Rotate();
         Thrust();
+        }       
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; } // ignore collisions when dead
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("ok");
                 break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f);
+                break;
             default:
-                print("dead");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
-
     }
 
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
     private void Thrust()
     {     
         if (Input.GetKey(KeyCode.Space)) // cam thrust while rotating
@@ -54,10 +74,7 @@ public class Rocket : MonoBehaviour
         }
     }
     private void Rotate()
-    {
-        rigidBody.freezeRotation = true; //take manual control of rotation
-
-        
+    {             
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
@@ -67,9 +84,7 @@ public class Rocket : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
-        }
-
-        rigidBody.freezeRotation = false; //resume physic control of rotation
+        }        
     }
 
     
